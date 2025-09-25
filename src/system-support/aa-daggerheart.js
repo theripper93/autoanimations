632bcf9a420d891560270d59f6612036eba73d1c
JamesERyan <104259981+JamesERyan@users.noreply.github.com>
2025-09-25 11:03:06 +0100

    aa daggerheart support

---
+++ src/system-support/aa-daggerheart.js
@@ -0,0 +1,41 @@
+import { trafficCop }       from "../router/traffic-cop.js"
+import AAHandler            from "../system-handlers/workflow-data.js";
+import { getRequiredData }  from "./getRequiredData.js";
+
+
+export function systemHooks() {
+  Hooks.on("createChatMessage", async (msg) => {
+  if(msg.type != "dualityRoll"){
+    return;
+  }
+  let data2 = msg.system ?? msg.flags?.daggerheart;
+    checkDHMessage(data2);
+  });
+}
+
+async function checkDHMessage(msg) {
+  if(!msg.source.item){
+    return;
+  }
+  let compiledData = await getRequiredData({
+      name: msg.title,
+      item: getItemDH(msg.source.item, msg.source.actor),
+      actorId: fromUuidSync(msg.source.actor),
+      targets: getTargetsDH(),///msg.targets,
+      workflow: msg
+  });
+  const handler = await AAHandler.make(compiledData);
+  trafficCop$1(handler);
+  
+}
+    function getTargetsDH(){
+      const targetarray = Array.from(game.user.targets);
+
+      return targetarray;
+    }
+
+    function getItemDH(selection, source) {
+        const actor = fromUuidSync(source);
+        let item = actor.items.find(i => i._id == selection);
+        return item;
+    }
